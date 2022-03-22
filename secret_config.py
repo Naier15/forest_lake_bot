daa93de0d4ac772d70804e74eed8c06702f091eb
Naier15 <67088318+Naier15@users.noreply.github.com>
2022-03-23 00:03:54 +1000

    Add files via upload

---
+++ secret_config.py
@@ -0,0 +1,10 @@
+TOKEN = '5233368911:AAGv8iiAx52cakNU4O_9DKw_L2fM6_GPbP8'
+
+user = 'root' #'postgres'
+password = 'naierlink'
+host = '127.0.0.1'
+port = '3306' #'5432' for postres
+database = 'forest_lake'
+
+secret_key_for_writer = 'editor22'
+secret_key_for_worker = 'staff22'
